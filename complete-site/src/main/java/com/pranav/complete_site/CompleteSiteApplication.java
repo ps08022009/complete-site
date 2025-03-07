e1346d72871bdd377d3a51bb6532d64f1b14b65d
sneha <sneha@LAPTOP-S7OVFFAT.(none)>
2025-03-06 21:44:57 -0800

    update

---
+++ complete-site/src/main/java/com/pranav/complete_site/CompleteSiteApplication.java
@@ -0,0 +1,13 @@
+package com.pranav.complete_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompleteSiteApplication.class, args);
+	}
+
+}
---
+++ complete-site/src/test/java/com/pranav/complete_site/CompleteSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranav.complete_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompleteSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
